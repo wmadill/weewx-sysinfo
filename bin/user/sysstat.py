bc67bed60f758be8d998f030d0ca7a069670cad6
Bill Madill <bill@jamimi.com>
2024-11-12 10:06:23 -0800

    Update for memory capture; converted unit test

--- bin/user/sysstat.py
+++ bin/user/sysstat.py
@@ -1,4 +1,6 @@
 # Copyright 2013-2013 Matthew Wall
+# Further copyright 2024 Bill Madill
+
 #### THIS IS BEING WRITTEN AND EVERYTHING IN IT IS
 #### NOT TO BE RELIED ON!
 """weewx module that records process information.
@@ -12,38 +14,43 @@ Configuration
 
 Add the following to weewx.conf:
 
-[ProcessMonitor]
-    data_binding = pmon_binding
+[SystemStatistics]
+    data_binding = sysstat_binding
+    process = sysstat
+    ##FIXME## add all the desired filesystems for sizing
 
 [DataBindings]
-    [[pmon_binding]]
-        database = pmon_sqlite
-        manager = weewx.manager.Manager
+    [[sysstat_binding]]
+        database = sysstat_sqlite
+        manager = weewx.manager.DayManager
         table_name = archive
-        schema = user.pmon.schema
+        schema = user.sysstat.schema
 
 [Databases]
-    [[pmon_sqlite]]
-        database_name = archive/pmon.sdb
+    [[sysstat_sqlite]]
+        database_name = sysstat.sdb
         database_type = SQLite
 
 [Engine]
     [[Services]]
-        archive_services = ..., user.pmon.ProcessMonitor
+        archive_services = ..., user.sysstat.SystemStatistics
 """
 
+### Try commenting each of these out to see what is really used...
 import logging
 import os
-import re
+# import re
 import time
-from subprocess import Popen, PIPE
+import resource
 
+# import weewx
 import weedb
 import weewx.manager
 from weeutil.weeutil import to_int
 from weewx.engine import StdService
+from weewx.cheetahgenerator import SearchList
 
-VERSION = "0.7"
+VERSION = "0.1"
 
 log = logging.getLogger(__name__)
 
@@ -51,23 +58,23 @@ schema = [
     ('dateTime', 'INTEGER NOT NULL PRIMARY KEY'),
     ('usUnits', 'INTEGER NOT NULL'),
     ('interval', 'INTEGER NOT NULL'),
-    ('mem_vsz', 'INTEGER'),
+    ('mem_size', 'INTEGER'),
     ('mem_rss', 'INTEGER'),
+    ('mem_share', 'INTEGER'),
 ]
-
 
 class SystemStatistics(StdService):
 
     def __init__(self, engine, config_dict):
-        super(ProcessMonitor, self).__init__(engine, config_dict)
+        super(SystemStatistics, self).__init__(engine, config_dict)
 
-        # To make what follows simpler, isolate the "pmon" part of the configuration file
-        pmon_dict = config_dict.get('ProcessMonitor', {})
-        self.process = pmon_dict.get('process', 'weewxd')
-        self.max_age = to_int(pmon_dict.get('max_age', 2592000))
+        d = config_dict.get('SystemStatistics', {})
+        self.process = d.get('process', 'weewxd')
+        self.max_age = to_int(d.get('max_age', 2592000))
+        self.page_size = resource.getpagesize()
 
         # get the database parameters we need to function
-        binding = pmon_dict.get('data_binding', 'pmon_binding')
+        binding = d.get('data_binding', 'sysstat_binding')
         self.dbm = self.engine.db_binder.get_manager(data_binding=binding,
                                                      initialize=True)
 
@@ -76,7 +83,7 @@ class SystemStatistics(StdService):
         dbm_dict = weewx.manager.get_manager_dict_from_config(config_dict, binding)
         memcol = [x[0] for x in dbm_dict['schema']]
         if dbcol != memcol:
-            raise Exception('pmon schema mismatch: %s != %s' % (dbcol, memcol))
+            raise Exception('sysstat schema mismatch: %s != %s' % (dbcol, memcol))
 
         self.last_ts = None
         self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
@@ -109,38 +116,55 @@ class SystemStatistics(StdService):
         sql = "delete from %s where dateTime < %d" % (self.dbm.table_name, ts)
         self.dbm.getSql(sql)
         try:
-            # sqlite databases need some help to stay small
             self.dbm.getSql('vacuum')
         except weedb.DatabaseError:
             pass
 
-    COLUMNS = re.compile(r'\S+\s+\d+\s+[\d.]+\s+[\d.]+\s+(\d+)\s+(\d+)')
-
     def get_data(self, now_ts, last_ts):
         record = {
             'dateTime' : now_ts,
-            'usUnits' : weewx.METRIC,
+            'usUnits' : weewx.US,
             'interval' : int((now_ts - last_ts) / 60.0)
         }
         try:
-            cmd = 'ps aux'
-            p = Popen(cmd, shell=True, stdout=PIPE)
-            o = p.communicate()[0].decode('ascii')
-            for line in o.split('\n'):
-                if line.find(self.process) >= 0:
-                    m = self.COLUMNS.search(line)
-                    if m:
-                        record['mem_vsz'] = int(m.group(1))
-                        record['mem_rss'] = int(m.group(2))
+            pid = os.getpid()
+            procfile = "/proc/%s/statm" % pid
+            try:
+                mem_tuple = open(procfile).read().split()
+            except (IOError, ):
+                return
+
+            # Unpack the tuple:
+            (size, resident, share, text, lib, data, dt) = mem_tuple
+
         except (ValueError, IOError, KeyError) as e:
-            log.error('apcups_info failed: %s' % e)
+            logerr('memory_info failed: %s' % e)
+
+        mb = 1024 * 1024
+        record['mem_size']  = float(size)     * self.page_size / mb 
+        record['mem_rss']   = float(resident) * self.page_size / mb
+        record['mem_share'] = float(share)    * self.page_size / mb
+
         return record
+
+class SystemStatisticsVariables(SearchList):
+    """Bind memory varialbes to database records"""
+
+    def __init__(self, generator):
+        SearchList.__init__(self, generator)
+
+    def version(self):
+        return VERSION
+
+    def get_extension_list(self, timespan, db_lookup):
+        """Returns a list of name/value pairs"""
+        return [{'sysstat': self}]
 
 
 # what follows is a basic unit test of this module.  to run the test:
 #
 # cd ~/weewx-data
-# PYTHONPATH=bin python bin/user/pmon.py
+# PYTHONPATH=bin python bin/user/sysstat.py
 #
 if __name__ == "__main__":
     from weewx.engine import StdEngine
@@ -148,9 +172,10 @@ if __name__ == "__main__":
     import weewx
 
     weewx.debug = 1
-    weeutil.logger.setup('pmon')
+    weeutil.logger.setup('sysstat')
 
     config = {
+        'WEEWX_ROOT': '/home/weewx/weewx-data',
         'Station': {
             'station_type': 'Simulator',
             'altitude': [0, 'foot'],
@@ -159,29 +184,32 @@ if __name__ == "__main__":
         'Simulator': {
             'driver': 'weewx.drivers.simulator',
             'mode': 'simulator'},
-        'ProcessMonitor': {
-            'data_binding': 'pmon_binding',
-            'process': 'pmon'},
+        'SystemStatistics': {
+            'data_binding': 'sysstat_binding',
+            'process': 'sysstat'},
         'DataBindings': {
-            'pmon_binding': {
-                'database': 'pmon_sqlite',
+            'sysstat_binding': {
+                'database': 'sysstat_sqlite',
                 'manager': 'weewx.manager.DaySummaryManager',
                 'table_name': 'archive',
-                'schema': 'user.pmon.schema'}},
+                'schema': 'user.sysstat.schema'}},
         'Databases': {
-            'pmon_sqlite': {
-                'database_name': 'pmon.sdb',
+            'sysstat_sqlite': {
+                'database_name': 'sysstat.sdb',
                 'database_type': 'SQLite'}},
         'DatabaseTypes': {
             'SQLite': {
                 'driver': 'weedb.sqlite',
-                'SQLITE_ROOT': '/var/tmp'}},
+                'SQLITE_ROOT': 'archive-dev'}},
         'Engine': {
             'Services': {
-                'process_services': 'user.pmon.ProcessMonitor'}}
+                'process_services': 'user.sysstat.SystemStatistics'}},
     }
+
+    # Logged entries are in syslog. View with journalctl --grep=sysstat
+
     eng = StdEngine(config)
-    svc = ProcessMonitor(eng, config)
+    svc = SystemStatistics(eng, config)
 
     nowts = lastts = int(time.time())
 
@@ -190,14 +218,12 @@ if __name__ == "__main__":
         while True:
             rec = svc.get_data(nowts, lastts)
             print(rec)
+            svc.save_data(rec)
             loop += 1
             if loop >= 3:
                 break
             time.sleep(5)
             lastts = nowts
             nowts = int(time.time()+0.5)
-    finally:
-        try:
-            os.remove('/var/tmp/pmon.sdb')
-        except FileNotFoundError:
-            pass
+    except:
+        pass
